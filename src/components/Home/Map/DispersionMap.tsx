31dee85cea3467045a54f2d20f377ee99d1d29d9
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-02-20 10:30:35 +0200

    [feat]: completed the login responsiveness

--- src/components/Home/Map/DispersionMap.tsx
+++ src/components/Home/Map/DispersionMap.tsx
@@ -1,21 +1,23 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import mapStyles from "./styles";
-import CustomMarker from "https://storage.cloud.google.com/track-master/public/bulk/marker.png";
 
 const DispersionMap = () => {
-  const mapsApiUrl = import.meta.env.VITE_API_URL
+  const mapsApiUrl = import.meta.env.VITE_API_URL;
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: mapsApiUrl
-  })
+    googleMapsApiKey: mapsApiUrl,
+  });
 
-  if (!isLoaded) return <img
-    src="logo.svg"
-    className="z-10 ml-[2rem] mt-8"
-    height={10}
-    width={80}
-    alt=""
-  />
+  if (!isLoaded)
+    return (
+      <img
+        src="logo.svg"
+        className="z-10 ml-[2rem] mt-8"
+        height={10}
+        width={80}
+        alt=""
+      />
+    );
 
   return (
     <div className="w-full h-fit">
@@ -24,16 +26,17 @@ const DispersionMap = () => {
         center={{ lat: 50.7127, lng: -73.9872 }}
         mapContainerClassName="map-container"
         options={{ styles: mapStyles }}
-
       >
         <Marker
           position={{ lat: 36.6163, lng: -100.61 }}
-          onClick={() => console.log('Marker clicked')}
-          options={{icon: CustomMarker}}
+          onClick={() => console.log("Marker clicked")}
+          options={{
+            icon: "https://storage.cloud.google.com/track-master/public/bulk/marker.png",
+          }}
         />
       </GoogleMap>
     </div>
-  )
-}
+  );
+};
 
 export default DispersionMap;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -9,9 +9,16 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="w-screen h-screen bg-primary flex overflow-hidden">
+    <div className="w-screen h-screen bg-primary flex overflow-hidden lMd:flex-col lMd:items-center lMd:justify-center">
+      <img
+        src="logo.svg"
+        className="z-10 mt-8 hidden lMd:block"
+        height={10}
+        width={80}
+        alt=""
+      />
       <div
-        className="w-full h-full bg-cover relative flex flex-col justify-between items-start md:hidden"
+        className="w-full h-full bg-cover relative flex flex-col justify-between items-start lMd:hidden"
         style={{
           backgroundImage: `url("/loginBg.png")`,
           backgroundPosition: "center",
@@ -20,16 +27,14 @@ const Login = () => {
         <div className="w-full h-full backdrop-blur-xl bg-[#000000b5] absolute"></div>
         <img
           src="logo.svg"
-          className="z-10 ml-[2rem] mt-8"
-          height={10}
-          width={80}
+          className="z-10 ml-[2rem] mt-8 w-[6rem] h-[6rem] 2lg:w-[4rem] 2lg:h-[4rem] 2lg:mt-[4rem]"
           alt=""
         />
         <div className="relative w-[80%] h-fit flex flex-col justify-start pl-[2rem] gap-[4rem]">
-          <h1 className="w-full text-7xl text-white font-bold flex flex-col gap-2">
+          <h1 className="w-full text-7xl text-white font-bold flex flex-col gap-2 2lg:text-5xl">
             <span>This is a test </span>
-            <span>piece that helps </span>
-            <span>designers and devs</span>
+            <span className="whitespace-nowrap">piece that helps </span>
+            <span className="whitespace-nowrap">designers and devs</span>
           </h1>
           <div className="w-[15rem] h-2 rounded-full flex gap-2">
             <div className="w-[40%] rounded-full bg-white"></div>
@@ -37,12 +42,12 @@ const Login = () => {
             <div className="w-full rounded-full bg-red"></div>
           </div>
         </div>
-        <div className="w-full h-fit flex z-10 justify-start gap-[5rem] pl-[5rem] mb-8">
+        <div className="w-full h-fit flex z-10 justify-start gap-[5rem] pl-[5rem] mb-8 2lg:pl-[2rem] 2lg:mb-[3rem] 2mlg:gap-[2rem]">
           {["Home", "About", "Contact Us", "Terms & Conditions"].map((item) => {
             return (
               <Link
                 to={item}
-                className="text-white font-bold text-sm uppercase"
+                className="text-white font-bold text-sm uppercase 2mlg:text-xs 2mlg:whitespace-nowrap"
               >
                 {item}
               </Link>
@@ -51,10 +56,10 @@ const Login = () => {
         </div>
       </div>
       <div className="w-full h-full flex flex-col items-center justify-center gap-[2rem]">
-        <span className="text-white text-4xl font-bold tracking-wider">
+        <span className="text-white text-4xl font-bold tracking-wider ssm1:text-3xl">
           Login to TrackMaster
         </span>
-        <div className="w-[50%] h-[5.5rem] flex flex-col items-start gap-2">
+        <div className="w-[50%] h-[5.5rem] flex flex-col items-start gap-2 ssm:w-[70%] ssm1:w-[80%] xss:w-full 2lg:w-[70%]">
           <span className="text-white text-sm font-semibold tracking-wider">
             Username
           </span>
@@ -77,7 +82,7 @@ const Login = () => {
             fullWidth
           />
         </div>
-        <div className="w-[50%] h-[5.5rem] flex flex-col items-start gap-2">
+        <div className="w-[50%] h-[5.5rem] flex flex-col items-start gap-2 ssm:w-[70%] ssm1:w-[80%] xss:w-full 2lg:w-[70%]">
           <span className="text-white text-sm font-semibold tracking-wider">
             Password
           </span>
@@ -104,7 +109,7 @@ const Login = () => {
             fullWidth
           />
         </div>
-        <div className="w-[48%] h-[1rem] flex justify-between md:w-[50%]">
+        <div className="w-[48%] h-[1rem] flex justify-between lMd:w-[50%] ssm:w-[70%] 2lg:w-[70%]">
           <FormControlLabel
             control={
               <Checkbox
@@ -129,14 +134,16 @@ const Login = () => {
             Reset password
           </Link>
         </div>
-        <button className="button hover:buttonOutline">Login</button>
+        <button className="button hover:buttonOutline ssm:w-[70%] 2lg:w-[70%] 2lg:hover:w-[70%]">
+          Login
+        </button>
         <h1 className="w-full flex flex-col items-center relative">
-          <hr className="w-[48%] border border-gray-400" />
+          <hr className="w-[48%] border border-gray-400 ssm:w-[70%] 2lg:w-[70%]" />
           <span className="text-gray-300 absolute bg-primary px-4 -translate-y-3">
             Or login with
           </span>
         </h1>
-        <div className="w-full h-fit flex items-center justify-center gap-4">
+        <div className="w-full h-fit flex items-center justify-center gap-4 ssm:gap-8">
           {["facebook", "google", "apple"].map((item) => {
             return (
               <div className="w-fit h-14 p-3 border rounded-xl border-gray-400">
@@ -149,7 +156,9 @@ const Login = () => {
             );
           })}
         </div>
-        <Link to="/signup" className="text-gray-400 tracking-wider">Haven't sign up yet? <b className="text-white">Sign Up</b></Link>
+        <Link to="/signup" className="text-gray-400 tracking-wider">
+          Haven't sign up yet? <b className="text-white">Sign Up</b>
+        </Link>
       </div>
     </div>
   );
