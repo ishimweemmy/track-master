a1fc968e400d7d632d0ef073f3366c60f93d6c4d
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-03-04 02:36:45 +0200

    [feat]: 85% completed of the integration is complete

--- src/services/data-api-slice.ts
+++ src/services/data-api-slice.ts
@@ -3,13 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const dataApiSlice = createApi({
   reducerPath: "dataApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8090",
+    baseUrl: "https://trackmaster.onrender.com/",
   }),
   endpoints: (builder) => {
     return {
       fetchAllCountries: builder.query({
         query() {
-          return `https://restcountries.com/v3.1/all?fields=flag,name,latlng`;
+          return `https://restcountries.com/v3.1/all?fields=flag,name`;
         },
       }),
       fetchAllVisitorsData: builder.query<any, number | void>({
